1a5689c11cd241b33b59394ff8f48496a09c70c6
LongtimenoseeQ <98392066+LongtimenoseeQ@users.noreply.github.com>
2026-06-17 15:35:09 +0800

    Add files via upload

---
+++ tkl_platform_types.h
@@ -0,0 +1,47 @@
+/**
+* @file tkl_platform_types.h
+* @brief This is tuya tkl platform types file
+* @version 0.1
+* @date 2021-04-06
+*
+* @copyright Copyright 2021-2022 Tuya Inc. All Rights Reserved.
+*
+*/
+
+#ifndef __TKL_PLATFORM_TYPES_H__
+#define __TKL_PLATFORM_TYPES_H__
+
+#include "tuya_cloud_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//Gpio Index 
+#define APP_GPIO_PA0    TUYA_GPIO_NUM_0
+#define APP_GPIO_PA1    TUYA_GPIO_NUM_1
+#define APP_GPIO_PA2    TUYA_GPIO_NUM_2
+#define APP_GPIO_PA3    TUYA_GPIO_NUM_3
+#define APP_GPIO_PA4    TUYA_GPIO_NUM_4
+#define APP_GPIO_PA5    TUYA_GPIO_NUM_5
+#define APP_GPIO_PA6    TUYA_GPIO_NUM_6
+#define APP_GPIO_PB0    TUYA_GPIO_NUM_7
+#define APP_GPIO_PB1    TUYA_GPIO_NUM_8
+#define APP_GPIO_PC0    TUYA_GPIO_NUM_9
+#define APP_GPIO_PC1    TUYA_GPIO_NUM_10
+#define APP_GPIO_PC2    TUYA_GPIO_NUM_11
+#define APP_GPIO_PC3    TUYA_GPIO_NUM_12
+#define APP_GPIO_PC4    TUYA_GPIO_NUM_13
+#define APP_GPIO_PC5    TUYA_GPIO_NUM_14
+#define APP_GPIO_PD0    TUYA_GPIO_NUM_15
+#define APP_GPIO_PD1    TUYA_GPIO_NUM_16
+#define APP_GPIO_PD2    TUYA_GPIO_NUM_17
+#define APP_GPIO_PD3    TUYA_GPIO_NUM_18
+#define APP_GPIO_PD4    TUYA_GPIO_NUM_19
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
